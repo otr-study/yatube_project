67a50f78902998c9fb9764f9bf0dc82021c8f13c
O. Trukhin <mr.truhinoleg@yandex.ru>
2021-11-19 07:18:08 -0800

    some refactoring

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -6,6 +6,6 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [
-    path('', views.index, name='main'),
-    path('group/<slug:slug>/', views.group_posts, name='group_detail_url'),
+    path('', views.index, name='index'),
+    path('group/<slug:slug>/', views.group_posts, name='group_list'),
 ]
